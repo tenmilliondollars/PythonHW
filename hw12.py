b10099de5d4d5158d8f80c5ef1a32eeee1bfa98e
Leonid Filatov <leonid-filatov@list.ru>
2021-03-06 19:00:16 +0300

    Made without recursion

--- hw12.py
+++ hw12.py
@@ -1,11 +1,9 @@
-n=int(input('Введите количество шагов:\n'))
-def f(n):  
-    if n == 0:
-        return 0  
-    if n == 1 or n==2:
-        return 1  
-    else:
-        return (f(n-1) + f(n-2))
-for i in range (n+1):
-    print(f(i))
-
+b=int(input('Введите количество шагов:\n'))
+n1 = 0
+n2 = 1
+n3 = 0
+for i in range(b+1):
+   n3 = n1 + n3
+   print(n3)
+   n1 = n2
+   n2 = n3
